3b91a8bf40df3fa02db0c52ab1bd7e30f402f4ff
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-26 16:52:31 +0100

    Added some basic comments to plan out UI

--- ProjectFiles/Main.hs
+++ ProjectFiles/Main.hs
@@ -12,3 +12,50 @@ import Demo
 import Functions
 import ScreenUtills
  
+--  MAIN
+-- Loads file and outputs a simplistic title.
+
+
+
+-- Options/Choice
+-- Outputs a simplistic menu which is recursive and allows a choice to be inputted by the user which calls the relevent choice function.
+
+
+
+-- Choice 1
+
+
+
+-- Choice 2
+
+
+
+-- Choice 3
+
+
+
+-- Choice 4
+
+
+
+-- Choice 5
+
+
+
+-- Choice 6
+
+
+
+-- Choice 7
+
+
+
+-- Choice 8
+
+
+
+-- Choice 9 (Quit)
+
+
+
+-- Error MSG
