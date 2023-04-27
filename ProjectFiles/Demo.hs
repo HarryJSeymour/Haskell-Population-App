b7fa01534a98ce0aa23e45ff95a99d1102a795aa
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-27 18:48:05 +0100

    Demo 4

--- ProjectFiles/Demo.hs
+++ ProjectFiles/Demo.hs
@@ -18,9 +18,8 @@ demo :: Int -> IO ()
 demo 1 = putStr(intercalate ", " (cityStrings testData) ++ "\n")
 demo 2 = putStrLn (specifiedCityPopulation testData "Berlin" 2022)
 demo 3 = putStrLn (generateTable testData)
-demo 4 = -- output the data (as for (iii)) after it has been updated with the
-         -- following new population figures (the first is for Amsterdam, etc.)
-         -- [1200,3200,3600,1800,9500,6800,11100,4300,2000,1800]
+demo 4 = putStrLn (generateTable (updatePopulationFigures testData 2023 [1200,3200,3600,1800,9500,6800,11100,4300,2000,1800]))
+
 demo 5 = -- show the data (as for (iii)) after adding "Stockholm" (59N, 18E) 
          -- with population figures [1657, 1633, 1608, 1583]
 demo 6 = -- output a list of annual growth figures for "Athens"
