bbc59629bd04198e9dfb13e689d47dba26a36c51
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-27 10:56:19 +0100

    Added a simplistic version of Task 2 with no error handling

--- ProjectFiles/Functions.hs
+++ ProjectFiles/Functions.hs
@@ -10,17 +10,27 @@ module Functions where
 -- Imports
 import Data -- Types / TestData
 import ScreenUtills -- Screen Utilities
+-- import Data.List
 
 -- Task 1
 -- Returns a string of all names of cities.
-cityStrings :: [City] -> [String]``
+cityStrings :: [City] -> [String]
 cityStrings cities = [ name | (name, _, _) <- cities]
 
 
 -- Task 2
 -- Returns the population of a specified city and a specified year (2023 = 0, 2022, = 1, etc), and return "No Data" if the year or city is invalid.
 -- Format the returned population to 3 decimal places with the first digit representing 1 million. "1.333m" with the m suffix.
+-- populationOnYear :: [City] -> String -> Int -> String
+-- populationOnYear cities conditionalName conditionalYear
 
+specifiedCityPopulation :: [City] -> String -> Int -> String
+specifiedCityPopulation cities conditionalName year = convertPopulation (([ (populations) | (name, _, populations) <- cities, name == conditionalName] !! 0) !! yearIndex year 0)
+-- Simple recursive function to find the index of the year (2023 = 0, 2022 = 1 ...)
+yearIndex :: Int -> Int -> Int
+yearIndex year n
+    | yearsList !! n == year = n
+    | otherwise = yearIndex year (n+1)
 
 
 -- Task 3
@@ -57,3 +67,26 @@ cityStrings cities = [ name | (name, _, _) <- cities]
 -- City Map, plots a visual map with the current years population figures.
 -- Terminal = (80 chars horizontal and 50 chars vertical).
 --  + Is used to id a citys location and population (1.333m) next to it.
+
+
+
+-- Helper Functions
+-- Converts a population figure to the correct format.
+convertPopulation :: Int -> String
+convertPopulation population = show ((fromIntegral population) / 1000) ++ "m"
+
+-- Converts a list of population figures to the correct format.
+convertPopulations :: [Int] -> [String]
+convertPopulations = map convertPopulation
+
+-- Accepts a city and returns its name.
+cityName :: City -> String
+cityName (name, _, _) = name
+
+-- Accepts a city and returns its cords.
+cityCords :: City -> (Int, Int)
+cityCords (_, cords, _) = cords
+
+-- Accepts a city and returns its populations.
+cityPop :: City -> [Int]
+cityPop (_, _, population) = population
