74998c47a94ea84981c09bce60994bd05cc6f7da
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-27 20:33:48 +0100

    Demo 6

--- ProjectFiles/Demo.hs
+++ ProjectFiles/Demo.hs
@@ -20,8 +20,10 @@ demo 2 = putStrLn (specifiedCityPopulation testData "Berlin" 2022)
 demo 3 = putStrLn (generateTable testData)
 demo 4 = putStrLn (generateTable (updatePopulationFigures testData 2023 [1200,3200,3600,1800,9500,6800,11100,4300,2000,1800]))
 demo 5 = putStrLn (generateTable (addNewCity testData ("Stockholm", (59, 18), [1657, 1633, 1608, 1583])))
-
-demo 6 = -- output a list of annual growth figures for "Athens"
-demo 7 = -- output the nearest city to location (45N, 8E) with 
-         -- a population above 4m people
-demo 8 = -- output the population map
+demo 6 = putStrLn (populationGrowthString ("Athens", (38, 24), [3153, 3153, 3154, 3156]))
+    
+    
+--      -- output a list of annual growth figures for "Athens"
+-- demo 7 = -- output the nearest city to location (45N, 8E) with 
+--          -- a population above 4m people
+-- demo 8 = -- output the population map
