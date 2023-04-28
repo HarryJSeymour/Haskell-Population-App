d81666e816b3d0d1c67dc0a72b433d74d24ccd0f
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-28 13:29:15 +0100

    changed input data for demo 2

--- ProjectFiles/Demo.hs
+++ ProjectFiles/Demo.hs
@@ -16,7 +16,7 @@ import Functions
 
 demo :: Int -> IO ()
 demo 1 = putStr(intercalate ", " (cityStrings testData) ++ "\n")
-demo 2 = putStrLn (specifiedCityPopulation testData "Berlin" 2022)
+demo 2 = putStrLn (specifiedCityPopulation testData "Berlin" 1)
 demo 3 = putStrLn (generateTable testData)
 demo 4 = putStrLn (generateTable (updatePopulationFigures testData 2023 [1200,3200,3600,1800,9500,6800,11100,4300,2000,1800]))
 demo 5 = putStrLn (generateTable (addNewCity testData ("Stockholm", (59, 18), [1657, 1633, 1608, 1583])))
