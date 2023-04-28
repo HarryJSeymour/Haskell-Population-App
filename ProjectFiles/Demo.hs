79513bc62225a1f5b1c2ef1af33580a7f04579c6
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-28 19:53:29 +0100

    Function renamed

--- ProjectFiles/Demo.hs
+++ ProjectFiles/Demo.hs
@@ -20,6 +20,6 @@ demo 2 = putStrLn (specifiedCityPopulation testData "Berlin" 1)
 demo 3 = putStrLn (generateTable testData)
 demo 4 = putStrLn (generateTable (updatePopulationFigures testData 0 [1200,3200,3600,1800,9500,6800,11100,4300,2000,1800]))
 demo 5 = putStrLn (generateTable (addNewCity testData ("Stockholm", (59, 18), [1657, 1633, 1608, 1583])))
-demo 6 = putStrLn (populationGrowthString ("Athens", (38, 24), [3153, 3153, 3154, 3156]))
+demo 6 = putStrLn (populationGrowth ("Athens", (38, 24), [3153, 3153, 3154, 3156]))
 demo 7 = putStrLn ((findNearestCity testData (45, 8) 4000))
 -- demo 8 = -- output the population map
