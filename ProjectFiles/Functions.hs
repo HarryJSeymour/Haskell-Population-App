40d64c17955e2c0b902ad2254513dac3d5eed1b5
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2023-04-28 19:34:09 +0100

    spacing and removal of old comments

--- ProjectFiles/Functions.hs
+++ ProjectFiles/Functions.hs
@@ -7,6 +7,8 @@ module Functions where
 --  Your functional code goes here
 --
 
+
+
 -- Imports
 import Data -- Types / TestData
 import ScreenUtills -- Screen Utilities
@@ -14,10 +16,12 @@ import Data.List
 import Text.Printf
 
 
+
 -- Task 1
 -- Returns a string of all names of cities.
 cityStrings :: [City] -> [String]
 cityStrings cities = [ name | (name, _, _) <- cities]
+
 
 
 -- Task 2
@@ -28,35 +32,44 @@ specifiedCityPopulation cities conditionalName yearsAgo = if (elem conditionalNa
     where
         populationLength = length (cityPop (cities !! 0))
 
-    
-    
+
 
 -- Task 3
 -- Returns a formatted string containing all the city data which when outputted using putStr will display as a five column table with a header,
 -- containing name, location (N & E), & this years and lasts population (formatted to 3 decimal places with a m suffix).
-
 generateTable :: [City] -> String
 generateTable cities = generateHeadings cities ++ generateTableContent cities cities
+
 
 
 generateHeadings :: [City] -> String
 generateHeadings cities = "Name" ++ spacing cities (totalLengthHeading cities - 4) ++ " | N  | E  | " ++ "Population \n" ++ spacing cities (totalLengthHeading cities + 8) ++ "   | 2023    | 2022\n"
 
+
+
 generateTableContent :: [City] -> [City] -> String
 generateTableContent cities [] = ""
 generateTableContent cities (x:xs) = (convertCityToString cities x) ++"\n"++ (generateTableContent cities xs) 
 
+
+
 convertCityToString :: [City] -> City -> String
 convertCityToString cities (name, cords, population) = name ++ spacing cities (totalLengthHeading cities - length name) ++" | "++cordsToString cords++" | " ++ populationsToStringpopulation population
 
+
+
 cordsToString :: (Int, Int) -> String
 cordsToString (x, y) = show x ++ (if x > 9 then "" else " ") ++" | "++ show y ++ (if y > 9 then "" else " ")
+
+
 
 populationsToStringpopulation :: [Int] -> String
 populationsToStringpopulation population = (if currentYear > 9.99 then "" else " ") ++ convertPopulationToString currentYear ++ " | " ++ (if previousYear > 9.99 then "" else " ") ++ convertPopulationToString previousYear
     where
         currentYear = convertPopulation(population !! 0)
         previousYear = convertPopulation(population !! 1)
+
+
 
 -- Spacing function ...
 spacing :: [City] -> Int -> String
@@ -76,6 +89,8 @@ updatePopulationFigures cities year populations = [ update year newPop city | (c
     where
         lists = zip cities populations
 
+
+
 -- Accepts a year, new population and city and returns a city with the new population added at the years position, and old populations pushed back.
 -- Years list is never updated !!!!
 update :: Int -> Int -> City -> City
@@ -83,15 +98,13 @@ update year newPopulation (a, b, populations) = (a, b, (take year populations ++
 
 
 
-
-
-        
-
 -- Task 5
 -- Adds a new city to the passed list of cities, with a similarly lengthed population list. (Added in an alphabetic order).
 -- Sort feature.
 addNewCity :: [City] -> City -> [City]
 addNewCity cities (name, cords, population) = if length population == length(cityPop(cities !! 0)) then sort (cities ++ [(name, cords, population)]) else error "Cities must have a similar amount of stored populations"
+
+
 
 -- Task 6
 -- Returns a list of yearly population growth figures.
@@ -99,22 +112,22 @@ addNewCity cities (name, cords, population) = if length population == length(cit
 -- Increase in population when compared to last year (0 <- 1), Increase in population for last year and the year before (1 <- 2) 
 -- The figures should show all years available. 
 -- The list should include negative numbers if the population is shrinking.
-
--- ("Amsterdam", (52,  5), [1158, 1149, 1140, 1132])
-
--- 0
--- [1132,1140,1149,1158]
---  0 1132 - 1140
 populationGrowth :: City -> City
 populationGrowth (name, cords, population) = (name, cords, (changeInt(reverse population) 0))
 
+
+
 populationGrowthString :: City -> String
 populationGrowthString (name, cords, population) = changeString(reverse population) 0
+
+
 
 changeInt :: [Int] -> Int -> [Int]
 changeInt populations n
     | n == length populations - 1 = []
     | otherwise = ((populations !! (n+1)) - (populations !! n)) : (changeInt populations (n+1))
+
+
 
 changeString :: [Int] -> Int -> String
 changeString populations n
@@ -123,19 +136,12 @@ changeString populations n
 
 
 
-
 -- Task 7
 -- Returns the name of the city closest to a specified location with a population higher than specified if no city can be found "No city" should be returned.
-    -- testData, ((45N, 8E) with a population above 4.000 (m) people
-
-
-
 findNearestCity :: [City] -> CityCoordinates -> Int -> String
 findNearestCity cities (n, e) population = fullCityString(cityList !! (findIndexOfMinimum 0 [pythagoreanConverter (name, cords) (n, e) | (name, cords, _) <- cityList]))
     where
         cityList = citiesOverPopulatin cities population
-
-
 
 
 
@@ -144,16 +150,15 @@ citiesOverPopulatin cities population = filter (\(_, _, pop) -> maximum pop > po
 
 
 
-
-
 findIndexOfMinimum :: Int -> [Float] -> Int
 findIndexOfMinimum n [] = 0
 findIndexOfMinimum n (x:xs) = if minimum (x:xs) == x then n else findIndexOfMinimum (n+1) xs
 
+
+
 -- Accepts a city coordinates and another set of coordinates and returns the distances between them.
 pythagoreanConverter :: (CityName, CityCoordinates) -> CityCoordinates -> Float
 pythagoreanConverter (name, (a,b)) (c, d) = sqrt(fromIntegral(c - a) ^2 + fromIntegral(d -b) ^2)
-
 
 
 
@@ -164,8 +169,7 @@ pythagoreanConverter (name, (a,b)) (c, d) = sqrt(fromIntegral(c - a) ^2 + fromIn
 
 
 
--- Helper Functions
-
+-- Helper functions, responsible for providing useful features required by the other functions or UI.
 fullCityString :: City -> String 
 fullCityString city = "Name: " ++cityName city ++ " | "++ cityCordsString city ++ intercalate ", " population
     where
@@ -173,32 +177,35 @@ fullCityString city = "Name: " ++cityName city ++ " | "++ cityCordsString city +
 
 
 
-
-
--- intercalate ", " (map convertPopulationToString (convertPopulations(cityPop ("Amsterdam", (52,  5), [1158, 1149, 1140, 1132]))))
--- Converts a population figure to the correct format.
--- 
 convertPopulationToString :: Float -> String
 convertPopulationToString population = (printf "%.3f" population) ++ "m"
+
+
 
 convertPopulation :: Int -> Float
 convertPopulation population = ((fromIntegral population) / 1000)
 
--- Converts a list of population figures to the correct format.
+
+
 convertPopulations :: [Int] -> [Float]
 convertPopulations = map convertPopulation
 
--- Accepts a city and returns its name.
+
+
 cityName :: City -> String
 cityName (name, _, _) = name
 
--- Accepts a city and returns its cords.
+
+
 cityCordsString :: City -> String
 cityCordsString (_, (n, e), _) = "N: " ++ show n ++ " | E: " ++ show e ++ " | "
+
+
 
 cityNameAndCords :: City -> (CityName, CityCoordinates)
 cityNameAndCords (name, (a, b), _) = (name, (a, b))
 
--- Accepts a city and returns its populations.
+
+
 cityPop :: City -> [Int]
 cityPop (_, _, population) = population
